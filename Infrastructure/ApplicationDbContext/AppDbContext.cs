198d2843f410caa5d91bdc6746e632258db93809
Alexey Franchuk <alexy.alexy98@mail.ru>
2024-04-29 00:36:04 +0300

    add infrastructures models presentation migrations

---
+++ Infrastructure/ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ModelsEntity;
+
+namespace ApplicationDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultDatabaseConnection"));
+        }
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Profession> Professions { get; set; }
+        public DbSet<ServiceCategoryMapping> ServiceCategoryMappings { get; set; }
+        public DbSet<CategoryProfessionMapping> CategoryProfessionMappings { get; set; }
+    }
+}
---
+++ Models/ModelsEntity/Profession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsEntity
+{
+    public class Profession
+    {
+        public int Id { get; set; }
+        public string NameOfProfession { get; set; } = "Название профессии";
+    }
+}

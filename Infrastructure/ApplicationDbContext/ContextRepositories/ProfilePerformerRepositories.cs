6ffc8cb01962a6823fce3b5c1c6617c1f4c8fbc3
Alexey Franchuk <alexy.alexy98@mail.ru>
2024-06-04 01:20:19 +0300

    #29 промежуточный коммит, создан репозиторий, интерфейс для PerformerProfile

---
+++ Infrastructure/ApplicationDbContext/ContextRepositories/ProfilePerformerRepositories.cs
@@ -0,0 +1,69 @@
+﻿using ApplicationDbContext.Interfaces;
+using ModelsEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDbContext.ContextRepositories
+{
+    public class ProfilePerformerRepositories : IProfilePerformerRepositories
+    {
+        private readonly AppDbContext context;
+
+        public ProfilePerformerRepositories(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        private async Task<User> FindUserById(int id)
+        {
+            if (id != 0)
+            {
+                return context.Users.Single(el => el.Id == id);
+            }
+            else
+            {
+                throw new ArgumentNullException("Error - id = null");
+            }
+
+        }
+
+        public async void CreateProfilePerformer(Performer performer, int userId)
+        {
+            User user = await FindUserById(userId);
+
+            if(user is not null && user.IsCustomer != true)
+            {
+                context.Performsers.Add(performer);
+                user.IsCustomer = true;
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new InvalidOperationException("Пользователя с таким id не найдено !!!");
+            }
+        }
+
+        public void DeleteProfilePerformerByPerformerId(int idperformerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Performer> GetProfilePerformer(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Performer> GetProfilePerformerByPerformerId(int performerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProfilePerformer(Performer performer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/ApplicationDbContext/Interfaces/IProfilePerformerRepositories.cs
@@ -0,0 +1,18 @@
+﻿using ModelsEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDbContext.Interfaces
+{
+    public interface IProfilePerformerRepositories
+    {
+        public void CreateProfilePerformer(Performer performer, int id);
+        public void UpdateProfilePerformer(Performer performer);
+        public void DeleteProfilePerformerByPerformerId(int performerId); // by performer Id 
+        public Task<Performer> GetProfilePerformer(int userId); //by user userId
+        public Task<Performer> GetProfilePerformerByPerformerId(int performerId); // by performer Id
+    }
+}

e8c759c0dda219bb1a70b382c0869c92e1cbeafa
Alexey Franchuk <alexy.alexy98@mail.ru>
2024-07-03 02:03:03 +0300

    #34 создал новые директории для view, создал кастомные layout настроил их маппинг с controller-ами

---
+++ Presentation/SiteEngine/Controllers/CustomerBoardController.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationDbContext;
+using ApplicationDbContext.Interfaces;
+using SiteEngine.Models.Amplua;
+using Microsoft.AspNetCore.Authorization;
+using ModelsEntity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SiteEngine.Controllers
+{
+    public class CustomerBoardController : BaseController
+    {
+        private readonly AppDbContext context;
+        private readonly IProfileCustomerRepositories profileCustomerRepositories;
+
+        public CustomerBoardController(AppDbContext context, IProfileCustomerRepositories profileCustomerRepositories)
+        {
+            this.context = context;
+            this.profileCustomerRepositories = profileCustomerRepositories;
+        }
+    }
+}
---
+++ Presentation/SiteEngine/Controllers/PerformerBoardController.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationDbContext;
+using ApplicationDbContext.Interfaces;
+using SiteEngine.Models.Amplua;
+using Microsoft.AspNetCore.Authorization;
+using ModelsEntity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SiteEngine.Controllers
+{
+    public class PerformerBoardController : BaseController
+    {
+        private readonly AppDbContext context;
+        private readonly IProfilePerformerRepositories profilePerformerRepositories;
+        private readonly IPerformerServiceMappingRepositories performerServiceMappingRepositories;
+        private readonly IServiceRepository serviceRepository;
+
+        public PerformerBoardController(AppDbContext context, 
+                                        IProfilePerformerRepositories profilePerformerRepositories, 
+                                        IPerformerServiceMappingRepositories performerServiceMappingRepositories, 
+                                        IServiceRepository serviceRepository)
+        {
+            this.context = context;
+            this.profilePerformerRepositories = profilePerformerRepositories;
+            this.performerServiceMappingRepositories = performerServiceMappingRepositories;
+            this.serviceRepository = serviceRepository;
+        }
+    }
+}

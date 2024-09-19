472a5ead43065289ff61ac163a0d4018cb15f70f
DESKTOP-15TSMLM\Виктория <afranchuk@sanatorium.team>
2024-09-19 19:25:09 +0300

    #14 Создал интерфейс IMessageRepositories

---
+++ Infrastructure/ApplicationDbContext/ContextRepositories/MessageRepositories.cs
@@ -0,0 +1,56 @@
+﻿using ApplicationDbContext.Interfaces;
+using ModelsEntity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDbContext.ContextRepositories
+{
+    public class MessageRepositories : IMessageRepositories
+    {
+        private readonly AppDbContext context;
+
+        public MessageRepositories(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Message> CreateMessage(Message message)
+        {
+            context.Messages.Add(message);
+            await context.SaveChangesAsync();
+            return message;
+        }
+
+        public async Task DeleteMessage(int messageId)
+        {
+            var message = await GetMessageByid(messageId);
+
+            if(message is not null)
+            {
+                context.Messages.Remove(message);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Message> GetMessageByid(int messageId)
+        {
+            return await context.Messages.FindAsync(messageId);
+        }
+
+        public async Task<List<Message>> GetMessagesByChatId(int chatId)
+        {
+            return await context.Messages.Where(el => el.ChatId == chatId).ToListAsync();
+        }
+
+        public async Task UpdateMessage(Message message)
+        {
+            context.Messages.Update(message);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Infrastructure/ApplicationDbContext/Interfaces/IMessageRepositories.cs
@@ -0,0 +1,18 @@
+﻿using ModelsEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDbContext.Interfaces
+{
+    public interface IMessageRepositories
+    {
+        Task<Message> CreateMessage(Message message);
+        Task UpdateMessage(Message message);
+        Task DeleteMessage(int messageId);
+        Task<Message> GetMessageByid(int messageId);
+        Task<List<Message>> GetMessagesByChatId(int chatId);
+    }
+}

9ed73d0255a209e75d7e210671ba627f0fbaeb44
Stanislav <kempachi.dzaraki@yandex.ru>
2024-09-30 03:10:22 +0300

    #72 Реализовал три таблицы. ListAbout, ListAboutItem, TypeListDate

---
+++ Models/ModelsEntity/TypeListDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsEntity
+{
+    public class TypeListDate
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
